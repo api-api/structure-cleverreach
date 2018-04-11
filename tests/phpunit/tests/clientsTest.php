aeb9fdd6bc8fefb2983e972ff73d8afaee1b3b09
Sven Wagener <sven@awesome.ug>
2018-04-11 19:27:52 +0200

    Renamed testcase

--- tests/phpunit/tests/clientsTest.php
+++ tests/phpunit/tests/clientsTest.php
@@ -3,7 +3,7 @@
 require_once dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
 
 class IdentificationsTests extends Cleverreach_TestCase {
-	public function testAdd() {
+	public function testGetClients() {
 		$request = $this->apiapi->get_request_object( 'cleverreach', '/clients.json' );
 		$response = $this->apiapi->send_request( $request );
 
